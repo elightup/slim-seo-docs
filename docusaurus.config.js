817abde187196d845eb557eaaff6248c165f8179
Anh Tran <anhtnt@elightup.com>
2025-11-23 19:54:20 +0700

    Black Friday sale

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,12 +65,12 @@ const config = {
       colorMode: {
         respectPrefersColorScheme: true,
       },
-      // announcementBar: {
-      //   content: '🔥 Introducing Slim SEO Pro - One single plugin with all premium features. <a href="https://wpslimseo.com/products/slim-seo-pro/">Get it now</a>.',
-      //   backgroundColor: '#fed7aa',
-      //   textColor: '#7c2d12',
-      //   isCloseable: false,
-      // },
+      announcementBar: {
+        content: '🔥 Black Friday Sale - Get 40% OFF <a href="https://wpslimseo.com/products/slim-seo-pro/">Slim SEO Pro</a> until Nov 30.',
+        backgroundColor: '#fed7aa',
+        textColor: '#7c2d12',
+        isCloseable: false,
+      },
       navbar: {
         logo: {
           alt: 'Slim SEO Logo',
